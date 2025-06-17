dc01b8b6301edf784d783c179e11ca4169dd4afc
fray osorio <frayosorio@gmail.com>
2025-06-16 21:31:20 -0500

    Se crea el servicio cliente de SELECCIONES de la API

---
+++ src/core/servicios/seleccion.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Seleccion } from '../../shared/entidades/Seleccion';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SeleccionService {
+
+  private url: string;
+
+  constructor(private http: HttpClient) {
+    this.url = `${environment.urlBase}selecciones/`;
+  }
+
+  public listar(): Observable<Seleccion[]> {
+    return this.http.get<Seleccion[]>(`${this.url}listar`);
+  }
+}
